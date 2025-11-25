09b69e1765f17e3cd61ea700ed964ca44fa96e4f
Joaquina Mendoza <86637139+joaquina18@users.noreply.github.com>
2025-11-25 19:44:34 +0000

    versión preliminar

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "ECG HRV API is running"
+
+@app.route("/process_rr", methods=["POST"])
+def process_rr():
+    data = request.get_json()
+
+    if "rr" not in data:
+        return jsonify({"error": "Missing rr array"}), 400
+
+    rr = np.array(data["rr"])
+
+    if len(rr) < 2:
+        return jsonify({"bpm": None, "rmssd": None, "sdnn": None})
+
+    bpm = 60000 / np.mean(rr)
+
+    diff = np.diff(rr)
+    rmssd = np.sqrt(np.mean(diff**2))
+
+    sdnn = np.std(rr)
+
+    return jsonify({
+        "bpm": round(float(bpm), 2),
+        "rmssd": round(float(rmssd), 2),
+        "sdnn": round(float(sdnn), 2)
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
